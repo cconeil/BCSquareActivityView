7dad8eaac9e80a5593d232bef003fc8618e3efed
Chris O'Neil <cconeil5@gmail.com>
2014-07-29 00:44:01 -0700

    Initial Commit

---
+++ BCSquareActivityView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BCSquareActivityView
+//
+//  Created by Chris O'Neil on 7/29/14.
+//  Copyright (c) 2014 Chris O'Neil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
